d7551f7bf9d6cd616619c8b0a1c89d2936620fb1
Sandeep <sandeepsharma999000@gmail.com>
2024-08-08 22:21:08 +0530

    first commit

--- app/details/page.js
+++ app/details/page.js
@@ -20,8 +20,8 @@ const searchClient = algoliasearch(
 const index = searchClient.initIndex('products');
 
 export default function Home() {
-    const searchParams = useSearchParams();
-    const c = searchParams.get('c');
+    const searchParams = useSearchParams() ;
+    const c = searchParams.get('c') || '';
     console.log('searchParams',c)
 
   const [name, setName] = useState(c);
@@ -61,7 +61,7 @@ export default function Home() {
     <main className="flex  flex-col items-center justify-between p-5">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <Link href="../" className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          back &nbsp;
+          back &nbsp; 
 
         </Link>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
